422db249957159e74aa3de866158887503de5ec4
e.ahanj <eng.ahanj@gmail.com>
2023-08-11 16:27:19 +0330

    add new input22skssssssllllllll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,30 @@ export default function RootLayout({
         http-equiv="Permissions-Policy"
         content="interest-cohort=(), user-id=()"
       />
+      <Script strategy="afterInteractive" id="otp">
+        {`if ('OTPCredential' in window) {
+      window.addEventListener('DOMContentLoaded', e => {
+        const input = document.querySelector('input[autocomplete="one-time-code"]');
+        if (!input) return;
+        const ac = new AbortController();
+        const form = input.closest('form');
+        if (form) {
+          form.addEventListener('submit', e => {
+            ac.abort();
+          });
+        }
+        navigator.credentials.get({
+          otp: { transport:['sms'] },
+          signal: ac.signal
+        }).then(otp => {
+          input.value = otp.code;
+          if (form) form.submit();
+        }).catch(err => {
+          console.log(err);
+        });
+      });
+    }`}
+      </Script>
 
       <body className={inter.className}>{children}</body>
     </html>
--- src/components/Otp.tsx
+++ src/components/Otp.tsx
@@ -3,52 +3,52 @@
 import React, { useEffect, useState } from "react";
 
 const Otp = () => {
-  const [code, setCode] = useState("bbbb");
-  useEffect(() => {
-    const ac = new AbortController();
-    console.log(navigator.credentials);
+  const [code, setCode] = useState("ccc");
+  // useEffect(() => {
+  //   const ac = new AbortController();
+  //   console.log(navigator.credentials);
 
-    (navigator.credentials as any).get({
-      otp: { transport: ["sms"] },
-      signal: ac.signal,
-    });
-    if (global?.window && "OTPCredential" in global?.window) {
-      console.log("in OTPCredentials");
+  //   (navigator.credentials as any).get({
+  //     otp: { transport: ["sms"] },
+  //     signal: ac.signal,
+  //   });
+  //   if (global?.window && "OTPCredential" in global?.window) {
+  //     console.log("in OTPCredentials");
 
-      {
-        // global?.document.addEventListener("DOMContentLoaded", function (e) {
-        console.log("IT IS LOAD??????????");
+  //     {
+  //       // global?.document.addEventListener("DOMContentLoaded", function (e) {
+  //       console.log("IT IS LOAD??????????");
 
-        console.log("WebOTP API is called");
+  //       console.log("WebOTP API is called");
 
-        (navigator.credentials as any)
-          .get({
-            otp: { transport: ["sms"] },
+  //       (navigator.credentials as any)
+  //         .get({
+  //           otp: { transport: ["sms"] },
 
-            signal: ac?.signal,
-          })
-          .then((otp: any) => {
-            console.log(otp);
+  //           signal: ac?.signal,
+  //         })
+  //         .then((otp: any) => {
+  //           console.log(otp);
 
-            if (otp) {
-              setCode(otp.code);
-              ac.abort();
+  //           if (otp) {
+  //             setCode(otp.code);
+  //             ac.abort();
 
-              console.log("submit() is called");
-            }
+  //             console.log("submit() is called");
+  //           }
 
-            navigator?.credentials?.preventSilentAccess();
-          })
-          .catch((err: any) => {
-            console.log("INJA OFTAD");
+  //           navigator?.credentials?.preventSilentAccess();
+  //         })
+  //         .catch((err: any) => {
+  //           console.log("INJA OFTAD");
 
-            console.log(err);
-          });
-        // });
-      }
-    }
-  }, []);
-  console.log((global.window as any)?.OTPCredential);
+  //           console.log(err);
+  //         });
+  //       // });
+  //     }
+  //   }
+  // }, []);
+  // console.log((global.window as any)?.OTPCredential);
 
   // if (global.window) {
   //   if ("OTPCredential" in global.window) {
@@ -93,7 +93,7 @@ const Otp = () => {
   //   }
   // }
   return (
-    <div className="flex text-4xl flex-col  max-w-screen-sm ">
+    <div className="flex text-4xl flex-col w-full">
       <div className="flex w-full mb-3 px-5">
         <h1>Code: </h1>
         <h1> {code} </h1>
