e2270fc02a151db55a9b9f3e030a74c9582e1075
e.ahanj <eng.ahanj@gmail.com>
2023-08-11 17:21:21 +0330

    add scritps 2elw2ks

--- src/components/Otp.tsx
+++ src/components/Otp.tsx
@@ -94,89 +94,100 @@ const Otp = () => {
   //   }
   // }
 
-  // useEffect(() => {
-  //   if ("OTPCredential" in window) {
-  //     console.log("ALAHO AKBARRRRRRRRRRRRRRR11111111111111");
+  useEffect(() => {
+    if ("OTPCredential" in window) {
+      console.log("ALAHO AKBARRRRRRRRRRRRRRR11111111111111");
 
-  //     document.addEventListener("DOMContentLoaded", function (e) {
-  //       console.log("ALAHO AKBARRRRRRRRRRRRRRR");
-  //       const input = document.querySelector(
-  //         'input[autocomplete="one-time-code"]'
-  //       );
-  //       console.log("1", input);
-  //       if (!input) return;
+      document.addEventListener("DOMContentLoaded", function (e) {
+        console.log("ALAHO AKBARRRRRRRRRRRRRRR");
+        const input = document.querySelector(
+          'input[autocomplete="one-time-code"]'
+        ) as HTMLInputElement;
+        console.log("1", input);
+        if (!input) return;
 
-  //       const ac = new AbortController();
-  //       const form = input.closest("form");
-  //       if (form) {
-  //         form.addEventListener("submit", (e) => {
-  //           ac.abort();
-  //         });
-  //       }
-  //       navigator.credentials
-  //         .get({
-  //           otp: { transport: ["sms"] },
-  //           signal: ac.signal,
-  //         })
-  //         .then((otp) => {
-  //           console.log("OTP", otp);
-  //           input.value = otp.code;
-  //           if (form) form.submit();
-  //         })
-  //         .catch((err) => {
-  //           console.log("AZZZZZZZZ ERRRPR");
-
-  //           console.log(err);
-  //         });
-  //     });
-  //   }
-  // }, []);
-  var dclhandler = false;
-  if (global.document) {
-    if (global.document.readyState !== "loading") {
-      start();
-    } else {
-      dclhandler = true;
-      global.document.addEventListener("DOMContentLoaded", start);
-    }
-  }
-  function start() {
-    if (global.document && dclhandler) {
-      global.document.removeEventListener("DOMContentLoaded", start);
-    }
-    console.log("Start the site`s JS activities");
-    const ac = new AbortController();
-    (navigator.credentials as any)
-      .get({
-        otp: { transport: ["sms"] },
-
-        signal: ac?.signal,
-      })
-      .then((otp: any) => {
-        console.log(otp);
-
-        if (otp) {
-          setCode(otp.code);
-          ac.abort();
-
-          console.log("submit() is called");
+        const ac = new AbortController();
+        const form = input.closest("form");
+        if (form) {
+          form.addEventListener("submit", (e) => {
+            ac.abort();
+          });
         }
+        (navigator.credentials as any)
+          .get({
+            otp: { transport: ["sms"] },
+            signal: ac.signal,
+          })
+          .then((otp: any) => {
+            console.log("OTP", otp);
+            input.value = otp.code;
+            if (form) form.submit();
+          })
+          .catch((err: any) => {
+            console.log("AZZZZZZZZ ERRRPR");
 
-        navigator?.credentials?.preventSilentAccess();
-      })
-      .catch((err: any) => {
-        console.log("INJA OFTAD");
-
-        console.log(err);
+            console.log(err);
+          });
       });
-  }
+    }
+  }, []);
+  // var dclhandler = false;
+  // if (global.document) {
+  //   if (global.document.readyState !== "loading") {
+  //     start();
+  //   } else {
+  //     dclhandler = true;
+  //     global.document.addEventListener("DOMContentLoaded", start);
+  //   }
+  // }
+  // function start() {
+  //   if (global.document && dclhandler) {
+  //     global.document.removeEventListener("DOMContentLoaded", start);
+  //   }
+  //   console.log("Start the site`s JS activities");
+  //   const ac = new AbortController();
+  //   (navigator.credentials as any)
+  //     .get({
+  //       otp: { transport: ["sms"] },
+
+  //       signal: ac?.signal,
+  //     })
+  //     .then((otp: any) => {
+  //       console.log(otp);
+
+  //       if (otp) {
+  //         setCode(otp.code);
+  //         ac.abort();
+
+  //         console.log("submit() is called");
+  //       }
+
+  //       navigator?.credentials?.preventSilentAccess();
+  //     })
+  //     .catch((err: any) => {
+  //       console.log("INJA OFTAD");
+
+  //       console.log(err);
+  //     });
+  // }
   return (
     <div className="flex text-4xl flex-col w-full">
+      <Script id="flan" strategy="afterInteractive">
+        {`var inCloudFlare = true;
+window.addEventListener("DOMContentLoaded", function () {
+    inCloudFlare = false;
+});
+if (document.readyState === "loading") {
+    window.addEventListener("load", function () {
+        if (inCloudFlare) window.dispatchEvent(new Event("DOMContentLoaded"));
+    });
+}`}
+      </Script>
       <div className="flex w-full mb-3 px-5">
         <h1>Code: </h1>
         <h1> {code} </h1>
       </div>
-      <form className="w-full px-2">
+      <form className="w-[200px] px-2">
         <input
           autoComplete="one-time-code"
           type="text"
