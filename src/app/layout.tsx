352ad3b9c14f1be1c649acf91607fed36c6d8703
e.ahanj <eng.ahanj@gmail.com>
2023-08-21 10:57:12 +0330

    o-4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,6 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script type="text/javascript" src="/js/otp.js" />
       <body className={inter.className}>{children}</body>
     </html>
   );
