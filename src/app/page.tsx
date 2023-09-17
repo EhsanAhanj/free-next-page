1ce222fd6a69ecb2bf36a5bea645d4ef73594036
e.ahanj <eng.ahanj@gmail.com>
2023-09-17 14:50:37 +0330

    4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,8 @@ import Otp from "@/components/Otp";
 
 export default function Home() {
   return (
-    <main className="flex max-w-[100vw] min-h-screen flex-col items-center justify-between">
-      <div className="flex max-w-[300px] min-h-screen justify-center bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 items-center dark:from-inherit ">
-        <Otp />
-      </div>
+    <main className="flex h-screen w-screen min-h-screen flex-col items-center justify-center">
+      <Otp />
     </main>
   );
 }
