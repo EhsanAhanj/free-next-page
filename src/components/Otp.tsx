e456b2bdd0bfc1fed28df2042202acb107cb609f
e.ahanj <eng.ahanj@gmail.com>
2023-09-17 14:33:43 +0330

    1

--- src/components/Otp.tsx
+++ src/components/Otp.tsx
@@ -4,39 +4,52 @@ import Script from "next/script";
 import React, { useEffect, useState } from "react";
 
 const Otp = () => {
-  const [code, setCode] = useState("mmmm");
+  const [code, setCode] = useState("vvvvvv");
   useEffect(() => {
     const ac = new AbortController();
+    console.log(navigator.credentials, "1");
 
-    (navigator.credentials as any)
-      .get({
-        otp: { transport: ["sms"] },
+    navigator?.credentials &&
+      (navigator.credentials as any)
+        .get({
+          otp: { transport: ["sms"] },
 
-        signal: ac?.signal,
-      })
-      .then((otp: any) => {
-        console.log(otp);
-        if (otp) {
-          setCode(otp.code);
+          signal: ac?.signal,
+        })
+        .then((otp: any) => {
+          console.log(otp);
+          if (otp) {
+            console.log("RESIIIIIIIIIIIIIIIIFFFFFFFFFFFFFFFFFFFFFF");
+
+            // helper.setValue(otp?.code, true);
+            // helper.setTouched(true);
+            // submitForm();
+            ac.abort();
+
+            console.log("submit() is called");
+          }
+
+          // navigator?.credentials?.preventSilentAccess();
+        })
+        .catch((err: any) => {
           ac.abort();
-        }
+          console.log(err);
+        });
 
-        navigator?.credentials?.preventSilentAccess();
-      })
-      .catch((err: any) => {
-        ac.abort();
-        console.log(err);
-      });
-    // });
+    // return () => {
+    //     console.log('otp aborting');
+    //     navigator?.credentials?.preventSilentAccess();
+    //     ac.abort();
+    // };
   }, []);
 
   return (
-    <div className="flex text-4xl flex-col w-full">
+    <div className="flex text-4xl flex-col w-full max-w-[80vw]">
       <div className="flex w-full mb-3 px-5">
         <h1>Code: </h1>
         <h1> {code} </h1>
       </div>
-      <form className="w-[200px] px-2" id="primary-form">
+      <form className="w-full px-2" id="primary-form">
         <input
           autoComplete="one-time-code"
           type="text"
