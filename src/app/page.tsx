4e0011cfd548fef533f49305388f71f190725dd9
Abolfazl-HOME <98ranjbar@gmail.com>
2023-12-05 12:40:19 +0330

    delete otp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Otp from "@/components/Otp";
-
 export default function Home() {
   return (
     <main className="flex h-screen w-screen min-h-screen flex-col items-center justify-center">
-      <Otp />
+      {/* <Otp /> */}
+      <h1>hello v1</h1>
     </main>
   );
 }
