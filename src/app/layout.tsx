0f387d1e2ba405e0f52f4222e6136d90915db919
Abolfazl-HOME <98ranjbar@gmail.com>
2023-12-05 13:00:04 +0330

    fix bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 /* eslint-disable @next/next/no-sync-scripts */
 import "./globals.css";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Head from "next/head";
-import { useRouter } from "next/router";
-import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +10,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const router = useRouter();
   return (
     <html lang="en">
       <Head>
@@ -25,30 +21,6 @@ export default function RootLayout({
         />
         <meta name="theme-color" content="#ff7d85" />
         <link rel="manifest" href="/manifest.json" />
-        <link
-          rel="apple-touch-icon"
-          href={`${router.basePath}/app-icon/icon-512x512.png`}
-          key="apple"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href={`${router.basePath}/app-icon/icon-72x72.png`}
-          key="icon32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href={`${router.basePath}/app-icon/icon-72x72.png`}
-          key="icon16"
-        />
-        <link
-          rel="icon"
-          href={`${router.basePath}/favicon.ico`}
-          key="favicon"
-        />
       </Head>
       {/* <Script type="text/javascript" src="otp.js" /> */}
       <body className={inter.className}>{children}</body>
