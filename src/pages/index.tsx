f6a60cbe062545fe3451ad68da3a7465bf01e587
Abolfazl-HOME <98ranjbar@gmail.com>
2023-12-05 15:58:20 +0330

    push

--- src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex h-screen w-screen min-h-screen flex-col items-center justify-center">
-      {/* <Otp /> */}
-      <h1 className="text-yellow-400">hello v60</h1>
-    </main>
-  );
-}
--- worker/index.ts
+++ worker/index.ts
@@ -9,6 +9,45 @@ const util = require("./utils");
 
 util();
 
+self.addEventListener("push", function (event: any) {
+  const data = JSON.parse(event.data.text());
+  event.waitUntil(
+    (self as any).registration.showNotification(data.title, {
+      body: data.message,
+      icon: "/icons/android-chrome-192x192.png",
+    })
+  );
+});
+
+self.addEventListener("notificationclick", function (event: any) {
+  event.notification.close();
+  event.waitUntil(
+    (self as any).clients
+      .matchAll({ type: "window", includeUncontrolled: true })
+      .then(function (clientList: any) {
+        if (clientList.length > 0) {
+          let client = clientList[0];
+          for (let i = 0; i < clientList.length; i++) {
+            if (clientList[i].focused) {
+              client = clientList[i];
+            }
+          }
+          return client.focus();
+        }
+        return (self as any).clients.openWindow("/");
+      })
+  );
+});
+
+// self.addEventListener('pushsubscriptionchange', function(event) {
+//   event.waitUntil(
+//       Promise.all([
+//           Promise.resolve(event.oldSubscription ? deleteSubscription(event.oldSubscription) : true),
+//           Promise.resolve(event.newSubscription ? event.newSubscription : subscribePush(registration))
+//               .then(function(sub) { return saveSubscription(sub) })
+//       ])
+//   )
+// })
 // listen to message event from window
 self.addEventListener("message", (event) => {
   // HOW TO TEST THIS?
