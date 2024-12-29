5603106f85f3da884ce778f34dc0990b6ff60d7c
Ehsan.Aj <eng.ahanj@gmail.com>
2024-12-29 14:26:21 +0330

    Trigger rebuild

--- src/pages/api/ping.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-async function GET({ query }: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json({ ping: "pong" });
-}
-
-export default GET;
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Home = (props: Props) => {
+  return <div>Hello from Home</div>;
+};
+
+export default Home;
